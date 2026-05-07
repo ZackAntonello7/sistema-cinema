7ed2bb950c6dd2cf49c7e1f84d089565231435e2
Thierry Antonello Pengo <thierry.pengo@sou.unifeob.edu.br>
2026-05-07 20:22:30 -0300

    Create Filme class with attributes for a movie

---
+++ src/model/filme.py
@@ -0,0 +1,7 @@
+class Filme:
+    def __init__(self, titulo, duracao, genero, diretor, elenco):
+        self.titulo = titulo
+        self.duracao = duracao
+        self.genero = genero
+        self.diretor = diretor
+        self.elenco = elenco
